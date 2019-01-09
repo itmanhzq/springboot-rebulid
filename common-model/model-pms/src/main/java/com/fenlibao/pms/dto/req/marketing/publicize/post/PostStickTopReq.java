5ddbb6d87f118cbfae24d645bfef7b0a30a892cf
wangboran <wangboran@fenlibao.com>
2019-01-09 11:09:41 +0800

    修改置顶请求类

--- common-model/model-pms/src/main/java/com/fenlibao/pms/dto/req/marketing/publicize/post/PostStickTopReq.java
+++ common-model/model-pms/src/main/java/com/fenlibao/pms/dto/req/marketing/publicize/post/PostStickTopReq.java
@@ -16,7 +16,12 @@ import javax.validation.constraints.NotBlank;
 @Data
 @Api("PostStickTopReq[设置文章置顶请求类]")
 public class PostStickTopReq extends BaseReq {
+
     @ApiModelProperty(value = "是否置顶 0:是; 1:否", required = true)
     @NotBlank(message = "是否置顶不能为空")
     private Boolean topPlace;
+
+    @ApiModelProperty(value = "公告Id", required = true)
+    @NotBlank(message = "公告Id不能为空")
+    private String id;
 }
