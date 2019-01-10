2b6a7e61d9eda89bbef60430a448c374871471f6
lifulin <lifulin@fenlibao.com>
2019-01-10 14:13:30 +0800

    干掉sonar扫描出来不合规的代码

--- xinwang/src/main/java/com/fenlibao/xinwang/model/po/QueryProjectInformation.java
+++ xinwang/src/main/java/com/fenlibao/xinwang/model/po/QueryProjectInformation.java
@@ -2,10 +2,7 @@ package com.fenlibao.xinwang.model.po;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
