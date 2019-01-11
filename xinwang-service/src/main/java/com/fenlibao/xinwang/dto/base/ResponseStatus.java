2c560b815978334e65c65737e678437145923ebb
lifulin <lifulin@fenlibao.com>
2019-01-11 11:50:33 +0800

    update README.md

---
+++ xinwang-service/src/main/java/com/fenlibao/xinwang/dto/base/ResponseStatus.java
@@ -0,0 +1,23 @@
+package com.fenlibao.xinwang.dto.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 错误码需按照规范来定义
+ *
+ * @author Flynn
+ * @date 2018/11/3
+ */
+@Getter
+@AllArgsConstructor
+public enum ResponseStatus {
+
+    COMMON_OPERATION_SUCCESS("200", "操作成功"),
+    COMMON_OPERATION_FAIL("500", "失败");
+
+    private String code;
+
+    private String msg;
+
+}
