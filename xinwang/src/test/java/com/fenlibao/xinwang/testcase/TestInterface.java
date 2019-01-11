d0a8d9108f01c74ce653b95ca575cdf3f560f3d7
lifulin <lifulin@fenlibao.com>
2019-01-11 09:48:31 +0800

    干掉sonar扫描出来不合规的代码

--- xinwang/src/test/java/com/fenlibao/xinwang/testcase/TestInterface.java
+++ xinwang/src/test/java/com/fenlibao/xinwang/testcase/TestInterface.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cglib.beans.BeanMap;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
